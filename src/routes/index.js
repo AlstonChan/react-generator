e4c3a6ce768831f5d06f2e64608780c710284a9f
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2022-08-06 17:26:41 +0800

    Hope

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import styles from "../styles/homepage.module.scss";
+import react from "../../public/react.png";
+import webpack from "../../public/webpack.png";
+
+import ImageRender from "../lib/ImageRender";
+
+export default function Homepage() {
+  return (
+    <div className={styles.all}>
+      <header className={styles.header}>
+        <h1 className={styles.navHeader}>React Webpack Template</h1>
+      </header>
+      <main>
+        <div className={styles.container}>
+          <div className={styles.imgContainer}>
+            <ImageRender src={react} className={styles.reactSpin} />
+          </div>
+          <div className={styles.imgContainer}>
+            <ImageRender src={webpack} />
+          </div>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        Created by <a href="https://github.com/AlstonChan">AlstonChan</a>
+      </footer>
+    </div>
+  );
+}
